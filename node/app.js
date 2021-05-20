7637cd1c041ca1fb85de5aff6f5d7fc9b97d3ef2
kokochi66 <kokochikochi@gmail.com>
2021-05-20 17:13:35 +0900

    2021.05.20 테서랙트 모델 서버 적용

--- node/app.js
+++ node/app.js
@@ -1,33 +1,46 @@
 const express = require('express');
+const fs = require('fs')
 const app = express();
 let  { PythonShell }  =  require ( 'python-shell' )
 let pyshell = new PythonShell('test.py');
 
-var options = {
-    mode: 'text',           // 텍스트나 json파일을 주고받기 가능
-    pythonPath: '',         // 설치된 파이썬의 경로를 저장, Path에 등록해두었으면 지정할 필요 없음.
-    pythonOptions: ['-u'],
-    scriptPath: '',
-    args: ['value1', 'value2', 'value3']
-};
 
-// PythonShell.run('test.py', options, function (err, results) {
-// if (err) throw err;
-// console.log('results: %j', results);
+    // pyshell.send(base64str);
+    // pyshell.on('message', function (message) {
+    //     console.log(message)
+    // }); // python 파일에 입력을 보낼 수 있는 코드
 
-// }); // python파일을 실행하여 출력하는 코드
 
 app.get('/', (req,res) => {
-    
-    pyshell.send('hello');
-    pyshell.on('message', function (message) {
-        // received a message sent from the Python script (a simple "print" statement)
-        console.log(message);
-    }); // python 파일에 입력을 보낼 수 있는 코드
+    var base64str = base64_encode('img.png');
+    let options = {
+        mode: 'text',
+        pythonPath: '', // Python의 경로를 나타낸다
+        pythonOptions: ['-u'], // get print results in real-time
+        scriptPath: '', // Python Script가 있는 경로
+        args: base64str // Python Script에 넘겨줄 인자 목록
+      };
 
-    res.send('hello nodejs!');
+    PythonShell.run('test.py', options, function (err, results) {
+        if (err) throw err;
+        res.send(results);
+    }); // python파일을 실행하여 출력하는 코드
+    
 }); // url 접속 시, python 파일이 실행 됨 
 
 app.listen(3000, () => {
     console.log('server connecting :: 3000');
 });
+
+
+function base64_encode(file) {   
+    var bitmap = fs.readFileSync(file);  
+    return new Buffer(bitmap).toString('base64');  
+}  
+  
+function base64_decode(base64str, file) {  
+    var bitmap = new Buffer(base64str, 'base64');  
+    fs.writeFileSync(file, bitmap);  
+}  
+  
+
--- node/test.py
+++ node/test.py
@@ -1,2 +1,13 @@
-str2 = input();
-print(str2+" Python!");
+from PIL import Image
+from io import BytesIO
+import base64
+import sys
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'.\Tesseract-OCR\tesseract'
+
+
+image_string = base64.b64decode(sys.argv[1])
+img = Image.open(BytesIO(image_string))
+
+print(pytesseract.image_to_string(img))
